b24733356e0dead973835d7fe9f2bf93ad9f8c4b
pirimyash <pirimyash@rambler.ru>
2024-05-02 15:42:50 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 import telebot
-
+from background import keep_alive
 API_KEY = "588822033:AAHLDF8M0a-4cQcUN_SQnsaIUFpZ0ROcWhs"
 
 bot = telebot.TeleBot(API_KEY)
@@ -11,6 +11,6 @@ def hello_func(message):
 @bot.message_handler()
 def echo_func(message):
     bot.send_message(message.from_user.id, message.text)
-
+keep_alive()
 bot.polling()
 
